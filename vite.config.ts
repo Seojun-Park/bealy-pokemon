2b155306d3a420a0fc53371df2a7777f6f9e86d1
JinPark <jinchul112@gmail.com>
2023-10-24 09:17:03 +0200

    feat(resource): first init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+    open: true,
+    cors: false,
+  },
+  plugins: [react()],
+});
