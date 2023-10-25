4fe165b4afc319f4ccccdc8edfc37ce2e977778a
JinPark <jinchul112@gmail.com>
2023-10-25 14:25:11 +0200

    chore(config): add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Seojun-Park.github.io/bealy-pokemon.io/',
   server: {
     port: 3000,
     host: true,
